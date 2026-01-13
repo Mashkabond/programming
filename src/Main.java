0c31151d86418ae302ae5e7dfb574f4e6a9b0e13
mariabondarenko <mashkabond23@gmail.com>
2026-01-13 21:15:50 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret to see IDE suggestions.
+//TIP Press <shortcut actionId="Debug"/> to start debugging your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+    }
+}
